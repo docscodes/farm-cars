170b4949c0a15c2c4bad79cfbb20b16016b0d8ea
docscodes <docscodes@gmail.com>
2025-05-24 14:56:12 -0400

    connect to atlas

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,28 @@
 from fastapi import FastAPI
+from motor import motor_asyncio
+from config import BaseConfig
 
-app = FastAPI()
+
+settings = BaseConfig()
+
+
+async def lifespan(app: FastAPI):
+    app.client = motor_asyncio.AsyncIOMotorClient(settings.DB_URL)
+    app.db = app.client[settings.DB_NAME]
+    
+    try:
+        app.client.admin.command("ping")
+        print("Pinged your deployment. You have successfully connected to MongoDB!")
+    except Exception as e:
+        print(e)
+        
+    yield
+    app.client.close()
+
+
+app = FastAPI(lifespan=lifespan)
 
 
 @app.get("/")
 async def get_root():
-    return {"Message": "Root working"}
+    return {"Message": "Root working!"}
