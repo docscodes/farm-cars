79eb6353c52c107da7fb136ea302c7ec332583ed
docscodes <docscodes@gmail.com>
2025-05-24 21:24:45 -0400

    useLoaderData

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom";
 import RootLayout from "./layouts/RootLayout";
 import Car from "./pages/Car";
-import Cars from "./pages/Cars";
+import Cars, { carsLoader } from "./pages/Cars";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import NewCar from "./pages/NewCar";
@@ -12,7 +12,7 @@ const router = createBrowserRouter(
     <Route path="/" element={<RootLayout />}>
       <Route index element={<Home />} />
       <Route path="login" element={<Login />} />
-      <Route path="cars" element={<Cars />} />
+      <Route path="cars" element={<Cars />} loader={carsLoader} />
       <Route path="cars/:id" element={<Car />} />
       <Route path="new-car" element={<NewCar />} />
       <Route path="*" element={<NotFound />} />
--- frontend/src/pages/Cars.jsx
+++ frontend/src/pages/Cars.jsx
@@ -1,5 +1,32 @@
+import { useLoaderData } from "react-router-dom";
+import CarCard from "../components/CarCard";
+
 const Cars = () => {
-  return <div>Cars</div>;
+  const cars = useLoaderData();
+
+  return (
+    <div>
+      <h1>Available cars</h1>
+
+      <div className="md:grid md:grid-cols-3 sm:grid sm:grid-cols-2 gap-5">
+        {cars.map((car) => (
+          <CarCard key={car.id} car={car} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Cars;
+
+export const carsLoader = async () => {
+  const res = await fetch(`${import.meta.env.VITE_API_URL}/cars?limit=30`);
+
+  const response = await res.json();
+
+  if (!res.ok) {
+    throw new Error(response.message);
+  }
+
+  return response["cars"];
+};
