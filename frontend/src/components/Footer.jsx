08fbe3bda60f871d12a9a596e0ee6cebbc5549ff
docscodes <docscodes@gmail.com>
2025-05-27 14:56:27 -0400

    update fonts

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom"
 
 export const Footer = () => {
   return (
-    <footer className="flex justify-center items-center m-4 h-16 text-2xl text-blue-800 border-t-2">
+    <footer className="flex justify-center items-center m-4 h-16 text-xl text-blue-800 border-t-2">
       <p>© 2030 <Link to="/">FARM</Link>. All Rights Reserved.</p>
     </footer>
   )
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,13 +3,13 @@ import Logo from "../assets/react.svg";
 
 export const Header = ({ user, logout }) => {
   return (
-    <header className="flex justify-between items-center m-4 h-16 text-2xl text-blue-800 border-b-2">
-      <Link to="/" className="logo flex items-center gap-2">
+    <header className="flex justify-between items-center m-4 h-16 text-blue-800 border-b-2">
+      <Link to="/" className="logo flex items-center gap-2 text-2xl">
         <img src={Logo} alt="Logo" />
         <span>FARM</span>
       </Link>
       
-      <nav className="flex items-center gap-4">
+      <nav className="flex items-center gap-4 text-xl">
         <NavLink to="/">Home</NavLink>
         <NavLink to="/cars">Cars</NavLink>
         {user ? (
