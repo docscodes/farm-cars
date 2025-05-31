46c0def5e6a122d51fe9ac46676085158c33b3b9
docscodes <docscodes@gmail.com>
2025-05-31 13:25:56 -0400

    update header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -6,7 +6,7 @@ export const Header = ({ user, logout }) => {
     <header className="flex justify-between items-center m-4 h-16 text-blue-800 border-b-2">
       <Link to="/" className="logo flex items-center gap-2 text-2xl">
         <img src={Logo} alt="Logo" />
-        <span>FARM</span>
+        <span>FARM Cars</span>
       </Link>
       
       <nav className="flex items-center gap-4 text-xl">
